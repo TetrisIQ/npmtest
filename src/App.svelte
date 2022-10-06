7b381cfac619b78f477621c0863baf0b86c15918
alwerner <alexander.werner@bonprix.net>
2022-10-06 11:15:19 +0200

    test

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
   <h1>Hello {name}!</h1>
   <p>
     Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build awsome Svelte apps.
+    how to build Svelte apps.
   </p>
 </main>
 
